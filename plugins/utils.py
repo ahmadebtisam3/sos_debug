fbd588ca149e31006ce71cc74c28ef4781867769
ebtisam <ahmadebtisam3@gmail.com>
2023-01-02 16:33:20 +0500

    add midclt

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,5 +1,11 @@
 import os
+from middlewared.client import Client
 
 
 def run(command: str) -> str:
     return os.popen(command).read()
+
+
+def call(command: str, *args) -> dict:
+    with Client() as c:
+        return c.call(command, *args)
